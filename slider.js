4ea4184b48f070713703a5ffc6ce44023e8c4aca
manvelpapoyan <58630681+manvelpapoyan@users.noreply.github.com>
2020-05-28 20:18:57 +0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,54 @@
+const imageArr = ['./1.JPG', './2.JPG', './3.JPG', './4.JPG', './5.JPG', './6.jpeg','./7.JPG',]
+
+
+const image = document.getElementById('image')
+
+const nextButton = document.getElementById('next')
+nextButton.addEventListener('click', next)
+const previousButton = document.getElementById('previous')
+previousButton.addEventListener('click', previous)
+const playPauseButton = document.getElementById('playPause')
+playPauseButton.addEventListener('click', playPause)
+
+let i = 0;
+let bool = true;
+
+function next() {
+    ++i;
+    if(i===imageArr.length){
+        i=0
+    }
+    image.src = imageArr[i];
+
+    console.log(i)
+
+}
+
+function previous() {
+
+    --i;
+    if(i<0){
+        i=imageArr.length-1
+    }
+
+
+    console.log(i)
+    image.src = imageArr[i];
+
+
+
+}
+
+function playPause() {
+    if (bool) {
+        id = setInterval(next, 1000)
+        bool = false
+    } else {
+        clearInterval(id)
+        bool = true;
+    }
+
+
+
+
+}
